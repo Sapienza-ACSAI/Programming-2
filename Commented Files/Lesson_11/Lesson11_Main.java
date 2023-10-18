4000929eee324395179fc668446b10e1e60d7403
Leonardo Biason <64312343+ElBi21@users.noreply.github.com>
2023-10-18 17:57:04 +0200

    Add files via upload

---
+++ Commented Files/Lesson_11/Lesson11_Main.java
@@ -0,0 +1,17 @@
+package Lesson_11;
+
+public class Lesson11_Main {
+    public static void main(String[] args) {
+        System.out.println("------- Starting Anonymous Classes Test -------");
+        TestAnonymous.TestAnonymousClasses();
+
+        System.out.println("------- Starting Lambda Expressions Test 1 -------");
+        FunctioTest.TestFunctio();
+
+        System.out.println("------- Starting Lambda Expressions Test 2 -------");
+        LambdaTest.TestLambda();
+
+        System.out.println("------- Starting Closures Test -------");
+        Closures.TestClosures();
+    }
+}
